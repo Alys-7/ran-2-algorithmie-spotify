6d83e35164c55f2f403edc941f19a12b116977d8
Alys-7 <deprez.alais1@gmail.com>
2025-09-25 17:28:38 +0200

    feat(churn): add function for churn rate per subscription type

--- churn-global.py
+++ churn-global.py
@@ -1,15 +1,22 @@
 from spotify import dataset
 
-total = 0 #Nombre total d'utilisateurs
-churned = 0 #Nombre d'utilisateurs ayant quitté le service
 
-for record in dataset:
-    total += 1
-    if record["is_churned"] == '1':
-        churned += 1
+def churn_rate (subscription_type): #subscription_type = liste de types d'abonnement
+    total = 0 #Nombre total d'utilisateurs ayant l'abonnement subscription_type
+    churned = 0 #Nombre d'utilisateurs ayant l'abonnement subscription_type et quitté le service
 
-print(f"Total users: {total}")
-print(f"Churned users: {churned}")
+    for record in dataset:
+        if record["subscription_type"] in subscription_type:
+            total += 1
+            if record["is_churned"] == '1':
+                churned += 1
 
-percentage = round((churned / total) * 100, 2)
-print(f"Churn rate: {percentage}%")
+    percentage = round((churned / total) * 100, 2)
+    return percentage
+
+print(churn_rate(["Free"]))
+print(churn_rate(["Family"]))
+print(churn_rate(["Student"]))
+print(churn_rate(["Premium"]))
+print(churn_rate(["Free", "Family", "Student", "Premium"])) #Churn global
+print(churn_rate(["Free", "Student"]))
