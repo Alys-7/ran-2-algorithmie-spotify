fd57854f5407465495f5a30ad2a88217d9948203
Alys-7 <deprez.alais1@gmail.com>
2025-09-29 14:46:57 +0200

    feat(revive): add proportion risky account

--- revive.py
+++ revive.py
@@ -1,10 +1,13 @@
 from spotify import dataset
 
 user_to_revive = []
+count = 0
 
-for record in dataset:
-    if ((float(record["skip_rate"]) > 0.3 and int(record["listening_time"]) < 100) or 
-        (record["subscription_type"] == "Free" and record["offline_listening"] == '0' and int(record["ads_listened_per_week"]) > 20)):
-        user_to_revive.append(record["user_id"])
+for user in dataset:
+    if ((float(user["skip_rate"]) > 0.3 and int(user["listening_time"]) < 100) or 
+        (user["subscription_type"] == "Free" and user["offline_listening"] == '0' and int(user["ads_listened_per_week"]) > 20)):
+        user_to_revive.append(user["user_id"])
+        count += 1
 
 print(user_to_revive)
+print(round(count / 8000 * 100, 2))
