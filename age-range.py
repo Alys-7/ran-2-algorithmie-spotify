8a9dd84066c914053463d19a87d14f8f5a67494e
Alys-7 <deprez.alais1@gmail.com>
2025-09-29 14:17:03 +0200

    feat(age): add number sunbscription_type per age

--- age-range.py
+++ age-range.py
@@ -1,24 +1,50 @@
 from spotify import dataset
 
-count0_19 = 0
+count_19 = 0
 count20_35 = 0
 count36_50 = 0
 count51_ = 0
 
-for user in dataset:
-    if int(user["age"]) <= 19:
-        count0_19 += 1
-    
-    elif int(user["age"]) <= 35:
-        count20_35 += 1
-    
-    elif int(user["age"]) <= 50:
-        count36_50 += 1
-    
-    else:
-        count51_ += 1
+# Nombre d'utilsateurs par tranche d'âge
 
-print(f"Le nombre d'utilisateurs de 19 ans ou moins est de {count0_19}")
-print(f"Le nombre d'utilisateurs entre 20 et 35 ans est de {count20_35}")
-print(f"Le nombre d'utilisateurs entre 36 et 50 ans est de {count36_50}")
-print(f"Le nombre d'utilisateurs de 51 ans ou plus est de {count51_}")
+def number_users_per_age():
+    for user in dataset:
+        if int(user["age"]) <= 19:
+            count_19 += 1
+        
+        elif int(user["age"]) <= 35:
+            count20_35 += 1
+        
+        elif int(user["age"]) <= 50:
+            count36_50 += 1
+        
+        else:
+            count51_ += 1
+
+    return [count_19, count20_35, count36_50, count51_]
+
+
+
+# Nombre de subscription_type par tranche d'âge
+
+def number_subscription_type_per_age():
+    subscription_type_per_age = {"count_19": {"Free": 0, "Student": 0, "Premium":0, "Family":0}, "count20_35": {"Free": 0, "Student": 0, "Premium":0, "Family":0}, "count36_50": {"Free": 0, "Student": 0, "Premium":0, "Family":0}, "count51_": {"Free": 0, "Student": 0, "Premium":0, "Family":0}}
+
+    for user in dataset:
+        subscription_type = user["subscription_type"]
+        
+        if int(user["age"]) <= 19:
+            subscription_type_per_age["count_19"][subscription_type] += 1
+        
+        elif int(user["age"]) <= 35:
+            subscription_type_per_age["count20_35"][subscription_type] += 1
+        
+        elif int(user["age"]) <= 50:
+            subscription_type_per_age["count36_50"][subscription_type] += 1
+        
+        else:
+            subscription_type_per_age["count51_"][subscription_type] += 1
+    
+    return subscription_type_per_age
+
+print(number_subscription_type_per_age())
